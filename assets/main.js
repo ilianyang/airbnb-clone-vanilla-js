c08b037b8da6346a030f91672f8221a8dd6a3424
Ilian <ilianyang88@gmail.com>
2023-09-14 15:45:38 +0800

    message

---
+++ assets/main.js
@@ -0,0 +1,88 @@
+/*=============== CHANGE BACKGROUND HEADER ===============*/
+const scrollHeader = () => {
+  const header = document.getElementById("header");
+  // When the scroll is greater than 50 viewport height, add the scroll-header class to the header tag
+  this.scrollY >= 100
+    ? header.classList.add("scroll-header")
+    : header.classList.remove("scroll-header");
+};
+window.addEventListener("scroll", scrollHeader);
+
+/*=============== SLIDER ===============*/
+const carousel = document.querySelector(".carousel");
+firstImg = carousel.querySelectorAll("img")[0];
+arrowIcons = document.querySelectorAll(".slider-icon");
+
+let isDragStart = false,
+  isDragging = false,
+  prevPageX,
+  preScrollLeft,
+  positionDiff;
+
+const showHideIcons = () => {
+  // showing and hiding prev/next icon according to carousel scroll left value
+  let scrollWidth = carousel.scrollWidth - carousel.clientWidth; // getting max scrollable width
+  arrowIcons[0].style.color = carousel.scrollLeft == 0 ? "#F0F0F0" : "black";
+  arrowIcons[1].style.color =
+    carousel.scrollLeft >= scrollWidth ? "#F0F0F0" : "black";
+};
+
+arrowIcons.forEach((icon) => {
+  icon.addEventListener("click", () => {
+    let firstImgWidth = firstImg.clientWidth + 14; // getting first img width & adding 14 margin value
+    // if clicked icon is left, reduce width value from the carousel scroll left else add to it
+    carousel.scrollLeft += icon.id == "left" ? -firstImgWidth : firstImgWidth;
+    setTimeout(() => showHideIcons(), 60); // calling showHideIcons after 60ms
+  });
+});
+const autoSlide = () => {
+  // if there is no image left to scroll then return from here
+  if (
+    carousel.scrollLeft - (carousel.scrollWidth - carousel.clientWidth) > -1 ||
+    carousel.scrollLeft <= 0
+  )
+    return;
+  positionDiff = Math.abs(positionDiff); // making positionDiff value to positive
+  let firstImgWidth = firstImg.clientWidth + 14;
+  // getting difference value that needs to add or reduce from carousel left to take middle img center
+  let valDifference = firstImgWidth - positionDiff;
+  if (carousel.scrollLeft > prevScrollLeft) {
+    // if user is scrolling to the right
+    return (carousel.scrollLeft +=
+      positionDiff > firstImgWidth / 3 ? valDifference : -positionDiff);
+  }
+  // if user is scrolling to the left
+  carousel.scrollLeft -=
+    positionDiff > firstImgWidth / 3 ? valDifference : -positionDiff;
+};
+const dragStart = (e) => {
+  // updatating global variables value on mouse down event
+  isDragStart = true;
+  prevPageX = e.pageX || e.touches[0].pageX;
+  prevScrollLeft = carousel.scrollLeft;
+};
+const dragging = (e) => {
+  // scrolling images/carousel to left according to mouse pointer
+  if (!isDragStart) return;
+  e.preventDefault();
+  isDragging = true;
+  carousel.classList.add("dragging");
+  positionDiff = (e.pageX || e.touches[0].pageX) - prevPageX;
+  carousel.scrollLeft = prevScrollLeft - positionDiff;
+  showHideIcons();
+};
+const dragStop = () => {
+  isDragStart = false;
+  carousel.classList.remove("dragging");
+  if (!isDragging) return;
+  isDragging = false;
+  autoSlide();
+};
+carousel.addEventListener("mousedown", dragStart);
+carousel.addEventListener("touchstart", dragStart);
+document.addEventListener("mousemove", dragging);
+carousel.addEventListener("touchmove", dragging);
+document.addEventListener("mouseup", dragStop);
+carousel.addEventListener("touchend", dragStop);
+
+/*=============== IMAGE SWIPER ===============*/
